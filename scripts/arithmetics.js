859177fe5d08deba2745555f546853bd01767e37
iQuickGaming <iquickpublic@gmail.com>
2021-07-26 10:08:27 +0200

    partially working arithmetics calculation and result check

--- scripts/arithmetics.js
+++ scripts/arithmetics.js
@@ -25,6 +25,7 @@ function PageLoad()
     document.querySelector("#difficulty").innerHTML = sessionStorage.getItem("difficulty").toUpperCase();
 }
 
+
 function StartGame()
 {
     document.querySelector("#desc").style.animation = "descriptionfall 1.5s linear forwards";
@@ -34,43 +35,106 @@ function StartGame()
     document.querySelector("#playground").style.animation = "playgroundmoveup 1s linear forwards";
     document.querySelector("#questionscount").innerHTML = sessionStorage.getItem("questionscount");
     document.querySelector("#answerscount").innerHTML = answerscount.toString();
+    setInterval(UpdateTimer, 1000);
     UpdateTimer();
+    GenerateQuestion();
 }
 
+
+var score = 0;
+var operands = ["+","-","×","÷"];
+var specialoperands = ["ⁿ","√"];
+var firstoperand = "";
+var secondoperand = ""
+var operator = "";
+var result = "";
+var timeleft = 60;
 var answerscount = 0;
 
 function NextQuestion()
 {
-   if (answerscount != sessionStorage.getItem("questionscount"))
+   if (answerscount == sessionStorage.getItem("questionscount"))
    {
        EndGame();
    }
    else
    {
+       answerscount++;
+       document.querySelector("#answerscount").innerHTML = answerscount;
+       timeleft = 60;
+       UpdateTimer();
        GenerateQuestion();
    }
 }
 
-var result;
-var score;
-var operands = ["+","-","×","÷","ⁿ","√"];
-
 function GenerateQuestion()
 {
-    // TODO
+    firstoperand = (Math.floor(Math.random() * 10));
+    secondoperand = (Math.floor(Math.random() * 10));
+    operator = operands[Math.floor(Math.random() * 4)];
+
+    switch (sessionStorage.getItem("difficulty"))
+    {
+        case "easy":
+        {
+            document.querySelector("#question").innerHTML = (firstoperand  + " " + operator + " " + secondoperand).toString();
+            break;
+        }
+
+        case "medium":
+        {
+            break;
+        }
+
+        case "hard":
+        {
+            break;
+        }
+    }
 }
 
 function CheckResult()
-{
+{   
+    // calculate the result
+
+    switch (operator)
+    {
+        case "+":
+        {
+            result = (firstoperand + secondoperand);
+            break;
+        }
+
+        case "-":
+        {
+            result = (firstoperand - secondoperand);
+            break;
+        }
+
+        case "×":
+        {
+            result = (firstoperand * secondoperand);
+            break;
+        }
+
+        case "÷":
+        {
+            result = (firstoperand / secondoperand);
+            break;
+        }
+            
+    }
+
+    // confront the result
+
     if (document.querySelector("#answer").value == result)
     {
         score++;
-        NextQuestion();
+        console.log("Score: " + score);
     }
-    else
-    {
-        NextQuestion();
-    }
+
+    document.querySelector("#answer").value = "";
+    NextQuestion();
 }
 
 function EndGame()
@@ -78,16 +142,12 @@ function EndGame()
  // TODO
 }
 
-var timer;
-var timeleft = 60;
-
 function UpdateTimer()
 {
     if (timeleft >= 0)
     {
         document.querySelector("#timeleft").innerHTML = timeleft.toString();
         timeleft--;
-        timer = setTimeout(UpdateTimer, 1000);
     }
     else
     {
